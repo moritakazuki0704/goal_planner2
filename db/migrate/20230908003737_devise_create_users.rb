95621e5ac08e6b3a8819e71de86e0afb9e1e7eb1
EC2 Default User <ec2-user@ip-172-31-18-139.ec2.internal>
2023-09-08 01:04:18 +0000

    [add]devise userモデル作成 userテーブル作成 secret_wordでログイン機能作成 emailカラムを削除 新規登録動作確認

--- db/migrate/20230908003737_devise_create_users.rb
+++ db/migrate/20230908003737_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :secret_word,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -32,11 +32,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
+      t.string :planner_name,  null: false, default: "", unique: true
 
       t.timestamps null: false
     end
 
-    add_index :users, :email,                unique: true
+    add_index :users, :secret_word,                unique: true
     add_index :users, :reset_password_token, unique: true
     # add_index :users, :confirmation_token,   unique: true
     # add_index :users, :unlock_token,         unique: true
